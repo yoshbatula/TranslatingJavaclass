d9e525dcc98ed3304297d9d80f8751376aa6c96a
yoshbatula <yoshbatula2@gmail.com>
2025-10-29 01:36:33 +0800

    adding my practice for python atm machine

---
+++ Atm-Machine.py
@@ -0,0 +1,30 @@
+from Transactions import Transaction
+
+class ATM:
+
+    if __name__ == "__main__":
+        
+        account = Transaction(0)
+
+        while True:
+            print("\nATM Machine Simulation")
+            print("[1]: Check Balance")
+            print("[2]: Deposit Money")
+            print("[3]: Withdraw Money")
+            print("[4]: Exit")
+
+            choice = input("Select an option: ")
+
+            if choice == '1':
+                account.check_balance()
+            elif choice == '2':
+                amount = float(input("Enter the amount to deposit: "))
+                account.deposit(amount)
+            elif choice == '3':
+                amount = float(input("Enter the amount to withdraw: "))
+                account.withdraw(amount)
+            elif choice == '4':
+                print("Exiting...")
+                break  
+            else:
+                print("Invalid option selected.")
---
+++ Transactions.py
@@ -0,0 +1,21 @@
+class Transaction:
+
+    def __init__(self, balance=0):
+        self.balance = balance
+
+    def deposit(self, amount):
+        if amount <= 0:
+            print("You can't deposit a negative or zero amount.")
+        else:
+            self.balance += amount
+            print(f"Succesfully deposited: ${amount}")
+
+    def withdraw(self, amount):
+        if amount > self.balance:
+            print("Insufficient funds for this withdrawal.")
+        else:
+            self.balance -= amount
+            print(f"Succesfully withdraw: ${amount}")
+
+    def check_balance(self):
+        print(f"Current balance: ${self.balance}")
